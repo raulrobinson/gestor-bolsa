f493d2e30c3f66dd58a9accd2704e57fc1f38881
Munoz Luis <luisdavid.munoz@amaris.com>
2022-10-27 11:11:09 -0500

    - add palette color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,39 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    colors:{
-
-      'currentColor':'#F6F6F6',
-      'secondaryColor':'#019DF4',
-      'white':'#FFFFFF'
-
-
+    colors: {
+      'currentColor': '#F6F6F6',
+      'secondaryColor': '#019DF4',
+      'white': '#FFFFFF',
+      brown: {
+        100: '#F5F5F5',
+        200: '#D3D4D3',
+        300: '#B6B7B7',
+        400: '#86888C',
+        500: '#50535A',
+        600: '#313235',
+      },
+      blue: {
+        300: '#019DF4',
+        600: '#0b2739'
+      },
+      green: {
+        100: '#DEBCE4',
+        300: '#5cb615',
+        600: '#642A72'
+      },
+      purple: {
+        300: '#A13EA1'
+      },
+      magenta: {
+        300: '#E63780'
+      },
+      orange: {
+        300: '#EC6248'
+      },
+      yellow: {
+        300: '#F28D15'
+      }
     },
     extend: {},
   },

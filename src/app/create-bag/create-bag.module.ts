aec1c15f996ee682de309c7c2cc6efb5fd940608
Munoz Luis <luisdavid.munoz@amaris.com>
2022-11-02 11:33:53 -0500

    - add equipment bag

--- src/app/create-bag/create-bag.module.ts
+++ src/app/create-bag/create-bag.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './presentation/components/menu/menu.component';
 import { MoneyComponent } from './presentation/components/money/money.component';
+import { EquipmentComponent } from './presentation/components/equipment/equipment.component';
 
 
 
 @NgModule({
     declarations: [
         MenuComponent,
-        MoneyComponent
+        MoneyComponent,
+        EquipmentComponent
     ],
   exports: [
     MenuComponent,
-    MoneyComponent
+    MoneyComponent,
+    EquipmentComponent
   ],
     imports: [
         CommonModule

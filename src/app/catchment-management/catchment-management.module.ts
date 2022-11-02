2106f1354461f97d934f1da22220536bd5ae0e37
Luis Mejia <luis.mejia.ext@sodexo.com>
2022-11-02 13:46:41 -0500

    refactoring nav and header bar; creating captación gestión

---
+++ src/app/catchment-management/catchment-management.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuComponent } from './presentation/components/menu/menu.component';
+
+
+
+@NgModule({
+  declarations: [
+    MenuComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CatchmentManagementModule { }

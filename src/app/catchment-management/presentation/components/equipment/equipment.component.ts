41baf432683971e0b8a9eb3f8cc056971056920e
Munoz Luis <luisdavid.munoz@amaris.com>
2022-11-02 19:20:01 -0500

    fix bug

--- src/app/catchment-management/presentation/components/equipment/equipment.component.ts
+++ src/app/catchment-management/presentation/components/equipment/equipment.component.ts
@@ -1,12 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-catchment',
+  selector: 'app-catchment-management-equipment',
   templateUrl: './equipment.component.html',
   styleUrls: ['./equipment.component.scss']
 })
 export class EquipmentComponent implements OnInit {
-  isMenu: true;
+  isMenu = true;
 
   constructor() {
   }

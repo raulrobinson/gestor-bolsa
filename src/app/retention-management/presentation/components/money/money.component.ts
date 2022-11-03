daf4546315a786dec1d02c8c0c9d96f8b45e3690
Munoz Luis <luisdavid.munoz@amaris.com>
2022-11-03 15:06:09 -0500

    - rename component retention money

--- src/app/retention-management/presentation/components/money/money.component.ts
+++ src/app/retention-management/presentation/components/money/money.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-money',
+  selector: 'app-retention-management-money',
   templateUrl: './money.component.html',
   styleUrls: ['./money.component.scss']
 })
